3c1180926da399386a6988aa8970a98bd254043d
Bodz <jean.boulot@epitech.eu>
2015-11-21 07:29:37 +0100

    pour daminou

---
+++ Assets/Scripts/Level 4/AdditionnalVisions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdditionnalVisions : MonoBehaviour 
+{
+    VisionType.e_VisionType[] additionnalVisions = new VisionType.e_VisionType[2] {VisionType.e_VisionType.DEFAULT, VisionType.e_VisionType.DEFAULT};
+   
+    public VisionType.e_VisionType Red
+    {
+        get
+        {
+            return additionnalVisions[0];
+        }
+    }
+
+    public VisionType.e_VisionType Blue
+    {
+        get
+        {
+            return additionnalVisions[1];
+        }
+    }
+
+    public void addVision(VisionType.e_VisionType v)
+    {
+
+    }
+}
