986df29eb664efe518466f05525053d63ed999c9
Nicholas Grant <seigneurfarfadet@hotmail.fr>
2015-12-12 18:06:38 +0900

    Level 2 screen with instruction

---
+++ Assets/Scripts/Level 2/ScreenBehavior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using SynchronizerData;
+
+public class ScreenBehavior : MonoBehaviour
+{
+    public Material normal_view;
+    public Material look;
+    public float bpm = 120.0f;
+
+    private BeatObserver beatObserver;
+    private bool isEnabled;
+    private float elapsedTime;
+
+    void Start()
+    {
+        beatObserver = GetComponent<BeatObserver>();
+        isEnabled = false;
+        elapsedTime = 0.0f;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > 60.0f / bpm)
+        {
+            if (isEnabled)
+            {
+                gameObject.GetComponent<Renderer>().material = look;
+            }
+            else
+            {
+                gameObject.GetComponent<Renderer>().material = normal_view;
+            }
+            isEnabled = !isEnabled;
+            elapsedTime = 0.0f;
+        }
+    }
+}
