add3424c02af442c96a5e82f884866a7aeb24ecf
Damien Blanc <damien.blanc@epitech.eu>
2015-12-14 14:39:27 +0900

    Outro

---
+++ Assets/Scripts/Outro/TextOutro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextOutro : MonoBehaviour
+{
+    void Start()
+    {
+        SpawnText.printText("Congratulations, you have passed all the tests !");
+        SpawnText.printText("Thank you very much for you cooperation !");
+        SpawnText.printText("You will now be brought back outside of this place, enjoy the rest of your life :)");
+    }
+}
