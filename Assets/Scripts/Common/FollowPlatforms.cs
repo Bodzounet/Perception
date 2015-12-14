98c4eb7618bf04dc6707de5eb60c539d743b9c7c
Damien Blanc <damien.blanc@epitech.eu>
2015-12-14 18:46:30 +0900

    Adding platform following level 2

---
+++ Assets/Scripts/Common/FollowPlatforms.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlatforms : MonoBehaviour
+{
+    Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        if (other.tag == "platform")
+        {
+            tr.SetParent(other.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(other.name);
+        if (other.tag == "platform")
+        {
+            tr.parent = null;
+        }
+    }
+}
