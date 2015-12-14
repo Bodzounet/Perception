1a2788408a288d32c1dd79b901974d14ef6c5e2e
Bodz <jean.boulot@epitech.eu>
2015-12-14 06:34:59 +0100

    Pour Daminou

--- Assets/Scripts/Intro/SendTextOpenDoor.cs
+++ Assets/Scripts/Intro/SendTextOpenDoor.cs
@@ -7,11 +7,13 @@ public class SendTextOpenDoor : MonoBehaviour
 
 	void Start ()
     {
-        SpawnText.printText("Hi, subject #357.");
-        SpawnText.printText("You are here because your cognitive capacities have been judged high enough to participate in this experiment.");
-        SpawnText.printText("You have been provided a last generation augmented reality headpiece.");
-        SpawnText.printText("It's the thing on your head, letting you read my sweet sweet messages. :)");
-        SpawnText.printText("You may now start the tests. Don't worry, nothing is lethal. Good luck.");
+        SpawnText st = GameObject.Find("GUI_Text").GetComponent<SpawnText>();
+
+        st.printText("Hi, subject #357.");
+        st.printText("You are here because your cognitive capacities have been judged high enough to participate in this experiment.");
+        st.printText("You have been provided a last generation augmented reality headpiece.");
+        st.printText("It's the thing on your head, letting you read my sweet sweet messages. :)");
+        st.printText("You may now start the tests. Don't worry, nothing is lethal. Good luck.");
         Invoke("OpenDoor", 35);
     }
 
